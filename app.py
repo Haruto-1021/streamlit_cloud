8d6611134670e046bc8d57f081df4fd3b9b16683
Haruto-1021 <hs4831050@gmail.com>
2025-06-25 13:47:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,15 +6,17 @@
 
 import streamlit as st
 import os
+from datetime import date
 
 input_firstname = st.text_input('名前を入力してください')
 input_lastname = st.text_input('名字を入力してください')
-input_year = st.text_input('生まれ年を入力してください')
-input_month = st.text_input('生まれた年を入力してください')
-input_date = st.text_input('生まれた日を入力してください')
+select_year = st.selectbox('年',range(1990,2025))
+select_month = st.selectbox('月',range(1,12))_
+input_date = st.text_input('日')
 result=""
 
-if st.button('表示'):
-    result='「'+input_firstname+'」「'+input_lastname+'」の誕生日は「'+input_year+'」年「'+input_month+'」月「'+input_date+'」日です'
+if st.button('記号有で生成'):
+    result=''+input_firstname+''+input_lastname+'の誕生日は'+select_year+'年'+select_month+'月'+input_date+'日です'
     st.write(result)
-
+elif st.button('記号無で生成'):
+    result=''+input_firstname+''+input_lastname+'の誕生日は、'+select_year+'年'+select_month+'月'+input_date+'日です!'
