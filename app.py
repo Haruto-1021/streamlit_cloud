d282ac3f592ace6e76539a3c0bac2cfc00f19c16
Haruto-1021 <hs4831050@gmail.com>
2025-06-25 14:09:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ from datetime import date
 
 input_firstname = st.text_input('名前を入力してください')
 input_lastname = st.text_input('名字を入力してください')
-select_year = st.selectbox('年',(range(1990,2025)))
-select_month = st.selectbox('月',(range(1,12)))
+select_year = st.selectbox('年',(range(1990,2026)))
+select_month = st.selectbox('月',(range(1,13)))
 input_date = st.text_input('日')
 result=""
 
 if st.button('記号有で生成'):
-    result=''+input_firstname+''+input_lastname+'の誕生日は'+select_year+'年'+select_month+'月'+input_date+'日です'
+    result = str(input_firstname) + str(input_lastname) + 'の誕生日は' + str(select_year) + '年'+ str(select_month) + '月' + input_date + '日です'
     st.write(result)
 elif st.button('記号無で生成'):
-    result=''+input_firstname+''+input_lastname+'の誕生日は、'+select_year+'年'+select_month+'月'+input_date+'日です!'
+    result = str(input_firstname) + str(input_lastname) + 'の誕生日は、' + str(select_year) + '年'+ str(select_month) + '月' + input_date + '日です!'
+    st.write(result)
