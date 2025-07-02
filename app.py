cd5a4048c30c4ca4b41f3970ef1cb63eaf8e22e1
Haruto-1021 <hs4831050@gmail.com>
2025-07-02 13:27:11 +0900

    r070702

--- app.py
+++ app.py
@@ -23,12 +23,12 @@ with col2:
 if input_firstname == '' or input_lastname == '':
     st.write('名前を入力してください')
 else:
-    if int(input_date) < 0 or ((int(select_month) == 4 or int(select_month) == 6 or int(select_month) == 9 or int(select_month) == 11) and int(input_date) > 31):
+    if int(input_date) < 0 or int(input_date) > 31 or ((int(select_month) = 4 or int(select_month) = 6 or int(select_month) = 9 or int(select_month) = 11) and int(input_day) > 30) or (((int(select_year) % 4 == 0 and int(select_year) % 100 != 0) or int(select_year) % 400 == 0) and int(select_month) == 2 and int(input_date) > 29) or (not((int(select_year) % 4 == 0 and int(select_year) % 100 != 0) or int(select_year) % 400 == 0) and int(select_month) == 2 and int(input_date) > 28):
         st.write('正しい生年月日を入力してください')
     else:
         if button1:
             result = str(input_firstname) + str(input_lastname) + 'の誕生日は' + str(select_year) + '年'+ str(select_month) + '月' + input_date + '日です'
             st.write(result)
-        elif button2:
+        else:
             result = str(input_firstname) + str(input_lastname) + 'の誕生日は、' + str(select_year) + '年'+ str(select_month) + '月' + input_date + '日です!'
             st.write(result)
