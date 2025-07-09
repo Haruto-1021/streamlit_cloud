63de4bdf54dc11852423950da43ee77a3da1053e
Haruto-1021 <hs4831050@gmail.com>
2025-07-09 15:53:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -110,8 +110,10 @@ if button:
               name.append(change0(l0[1],v1,v2))
               x=change1(name,n0,w0)
               st.write(x)
-              st.write(x,('i','1'))
-              st.write(x,('o','0'))
+              st.write(x.('i','1'))
+              st.write(x.('o','0'))
+              st.write(x.('0','o'))
+              st.write(x.('1','i'))
               count+=1
             st.write(str(count) + '個のパスワードを生成しました')
                 
@@ -165,6 +167,6 @@ if button:
               name.append(change0(l0[1],v1,v2))
               x=change1(name,n0,w0)
               st.write(x)
-              st.write(x,replace('a','@'))
+              st.write(x.replace('a','@'))
               count+=1
             st.write(str(count) + '個のパスワードを生成しました')
