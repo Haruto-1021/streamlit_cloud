98f447dd653822059aa4c49b7222182d06d006c4
Haruto-1021 <hs4831050@gmail.com>
2025-11-12 15:49:01 +0900

    r0711121549

--- app2.py
+++ app2.py
@@ -247,7 +247,7 @@ if button:
                 return x0
             
               if s0[1] == '':
-              　 def change1(x0, x1, c0): # 文字列の置き換え
+                 def change1(x0, x1, c0): # 文字列の置き換え
                    x = ""
                    for i in range(len(cc)):
                      if cc[i] == "0":
@@ -322,7 +322,7 @@ if button:
                               st.write(str(st.session_state.count) + '個のパスワードを生成しました')
                               st.write(str(st.session_state.count) + '個のパスワードを生成するのに、str(end_time - start_time) + '秒かかりました')
               if s0[1] == '':
-              　 def change1(x0, x1, c0): # 文字列の置き換え
+                 def change1(x0, x1, c0): # 文字列の置き換え
                    x = ""
                    for i in range(len(cc)):
                      if cc[i] == "0":
